6726c7b1e0341fb695852f5ab785b1ed86541294
Jacelyn Thornsberry <thornsberryj16@gmail.com>
2021-10-22 17:20:04 -0400

    almost finished menu, finished space test and dao

--- src/main/java/com/techelevator/jdbcDAO/JDBCSpaceDAO.java
+++ src/main/java/com/techelevator/jdbcDAO/JDBCSpaceDAO.java
@@ -9,6 +9,9 @@ import org.springframework.jdbc.support.rowset.SqlRowSet;
 import javax.sql.DataSource;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.SignStyle;
+import java.time.temporal.ChronoField;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,7 +28,7 @@ public class JDBCSpaceDAO implements SpaceDAO {
     public List<Space> retrieveSpacesFromVenues(Long venueId) {
         List<Space> spaceList = new ArrayList<>();
 
-        SqlRowSet results = jdbcTemplate.queryForRowSet("SELECT * FROM space WHERE venue_id = ?", venueId);
+        SqlRowSet results = jdbcTemplate.queryForRowSet("SELECT id, venue_id, name, is_accessible, open_from, open_to, daily_rate::decimal(9,2), max_occupancy FROM space WHERE venue_id = ?", venueId);
 
         while (results.next()) {
             Long id = results.getLong("id");
@@ -43,16 +46,18 @@ public class JDBCSpaceDAO implements SpaceDAO {
         DateTimeFormatter formatDateToMonth = DateTimeFormatter.ofPattern("MM");
         LocalDate endDateNeeded = dateNeeded.plusDays(numberOfDays);
 
+
+
         String sql =
-        "SELECT * " +
+        "SELECT DISTINCT(space.id), space.venue_id, space.name, space.is_accessible, space.open_from, space.open_to, space.daily_rate::decimal(9,2), space.max_occupancy " +
         "FROM space " +
         "LEFT JOIN reservation ON reservation.space_id = space.id " +
         "WHERE space.venue_id = ? " +      // ? = venueId
-        "AND space.open_from <= ? " +      // ? = month of dateNeeded
-        "AND space.open_to >= ? " +        // ? = month of (dateNeeded + numberOfDays)
+        "AND (space.open_from <= ? OR space.open_from IS NULL) " +      // ? = month of dateNeeded
+        "AND (space.open_to >= ? OR space.open_from IS NULL) " +        // ? = month of (dateNeeded + numberOfDays)
         "AND space.max_occupancy >= ? " +  // ? = numberOfPeople
-        "AND ((?::DATE < reservation.start_date OR reservation.start_date IS NULL) AND (?::DATE > reservation.end_date OR reservation.end_date IS NULL)) " + // ?, ? = dateNeeded
-        "AND ((?::DATE > reservation.start_date OR reservation.start_date IS NULL) AND (?::DATE < reservation.end_date OR reservation.end_date IS NULL))";   // ?, ? = dateNeeded + numberOfDays
+        "AND (((?::DATE < reservation.start_date OR reservation.start_date IS NULL) AND (?::DATE < reservation.start_date OR reservation.start_date IS NULL)) " + // ?, ? = dateNeeded
+        "OR ((?::DATE > reservation.end_date OR reservation.end_date IS NULL) AND (?::DATE > reservation.end_date OR reservation.end_date IS NULL)))";   // ?, ? = dateNeeded + numberOfDays
 
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql,
                 venueId,
@@ -60,8 +65,8 @@ public class JDBCSpaceDAO implements SpaceDAO {
                 Integer.parseInt(endDateNeeded.format(formatDateToMonth)),
                 numberOfPeople,
                 dateNeeded.format(formatFullDate),
-                dateNeeded.format(formatFullDate),
                 endDateNeeded.format(formatFullDate),
+                dateNeeded.format(formatFullDate),
                 endDateNeeded.format(formatFullDate)
         );
 
@@ -75,7 +80,7 @@ public class JDBCSpaceDAO implements SpaceDAO {
 
     public Space retrieveSpaceDetails(Long spaceId) {
 
-        String sql1 = "SELECT * FROM space WHERE id = ? AND open_from IS NULL";
+        String sql1 = "SELECT id, venue_id, name, is_accessible, open_from, open_to, daily_rate::decimal(9,2), max_occupancy FROM space WHERE id = ? AND open_from IS NULL";
         SqlRowSet result1 = jdbcTemplate.queryForRowSet(sql1, spaceId);
             if (result1.next()) {
                 return new Space(
@@ -90,7 +95,7 @@ public class JDBCSpaceDAO implements SpaceDAO {
                 );
             }
 
-        String sql2 = "SELECT * FROM space WHERE id = ? AND open_from IS NOT NULL";
+        String sql2 = "SELECT id, venue_id, name, is_accessible, open_from, open_to, daily_rate::decimal(9,2), max_occupancy FROM space WHERE id = ? AND open_from IS NOT NULL";
         SqlRowSet result2 = jdbcTemplate.queryForRowSet(sql2, spaceId);
         if (result2.next()) {
             return new Space(
