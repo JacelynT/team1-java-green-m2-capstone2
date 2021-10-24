afe3c890f2ed08e0876d403ac4cac0da02569453
Joel Kundla <leojaldnuk@gmail.com>
2021-10-23 22:06:52 -0400

    corrected Reservation Interface

--- src/main/java/com/techelevator/ReservationDAO.java
+++ src/main/java/com/techelevator/ReservationDAO.java
@@ -1,7 +1,9 @@
 package com.techelevator;
 
+import java.time.LocalDate;
+
 public interface ReservationDAO {
 
-
+    Reservation addReservation(Long spaceId, LocalDate date, int numberOfDays, int attendance, String reservationFor);
 
 }
